e9aea170e5d52f333591050d498e4288e2a39d7b
oh <osn9274@naver.com>
2024-09-22 21:26:38 +0900

    날짜

---
+++ src/time/IsSupportMain1.java
@@ -0,0 +1,15 @@
+package time;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+
+public class IsSupportMain1 {
+
+    public static void main(String[] args) {
+
+        LocalDate now = LocalDate.now();
+        int minute = now.get(ChronoField.SECOND_OF_MINUTE);
+
+        System.out.println("minute = " + minute);
+    }
+}
